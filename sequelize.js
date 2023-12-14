55c9e5fa1f3a829f16585ee9911c4b56d1593554
AmandaKly <amandaklly0@gmail.com>
2023-12-14 17:39:06 -0300

    Alterando variáveis

--- sequelize.js
+++ sequelize.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize({
   host: 'projetokubernetes.postgres.database.azure.com',
   username: 'atv',
   password: 'Atividade123!',
-  database: 'produtoshomolog',
+  database: 'produtosproduction',
   port: 5432,
   logging: false,
   dialectOptions: {
