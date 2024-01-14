8103e7a896ea1ca2bbbc6ccc42cc3383f65e115b
AmandaKly <amandaklly0@gmail.com>
2024-01-14 13:36:41 -0300

    Alterando variaveis de ambiente

--- sequelize.js
+++ sequelize.js
@@ -2,16 +2,16 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT,
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  logging: process.env.DB_LOGGING === 'true',
+  logging: false,
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true',
+      rejectUnauthorized: false, // Use apenas se estiver enfrentando problemas de certificado
     },
   },
 });
